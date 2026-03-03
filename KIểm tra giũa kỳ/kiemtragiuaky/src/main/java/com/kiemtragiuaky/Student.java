5f6006be00c4d1425ad7d98ebae223f4d69e7348
LeQuyen5826 <lequyen.5826@gmail.com>
2026-03-03 17:35:08 +0700

    Add files via upload

---
+++ KIểm tra giũa kỳ/kiemtragiuaky/src/main/java/com/kiemtragiuaky/Student.java
@@ -0,0 +1,29 @@
+
+package com.kiemtragiuaky;
+
+
+public class Student {
+    private String id;
+    private String name;
+    private double gpa;
+    
+    public Student(String id,String name,double gpa){
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+    
+    public String getId() {return id ;}
+    public String getName() {return name ;}
+    public double getGpa() {return gpa ;}
+    
+    public String setId() {return id ;}
+    public String setName() {return name ;}
+    public double setGpa() {return gpa ;} 
+  
+    @Override
+    public String toString() {
+        return "Student{id='" + id + "', name='" + name + "', gpa=" + gpa + "}";
+
+    }
+}
---
+++ KIểm tra giũa kỳ/kiemtragiuaky/src/main/java/com/kiemtragiuaky/StudentManager.java
@@ -0,0 +1,19 @@
+
+package com.kiemtragiuaky;
+
+import java.util.ArrayList;
+import java.util.List;
+        
+public class StudentManager<T> {
+    private List<T> data;
+    
+    public StudentManager() {
+        this.data = new ArrayList<>();
+    }
+    public void add(T item){
+        data.add(item);
+    }
+    public List<T> getAll(){
+        return new ArrayList<>(data);
+    }
+}
